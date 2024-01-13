b40af11a930ac422f60c0768d44041595ae2f651
KrishnaSabat <202003053.krishnasrs@student.xavier.ac.in>
2024-01-13 10:52:50 +0530

    amazon clone by using html and css

--- JAVASCRIPT/hoisting.js
+++ JAVASCRIPT/hoisting.js
@@ -34,11 +34,33 @@
 // m();
 // --------------------------------------------------CLOSURES---------------------------------------------------
 //]function and lexical scope bundled together  
-function l(){
-    var a = 7;
-    function p(){
-        console.log(a);
-    }
-    p();
-}
-l();
+// function l(){
+//     var a = 7;
+//     function p(){
+//         console.log(a);
+//     }
+//     p();
+// }
+// l();
+// -------------------------------------------------FIRST CLASS FUNCTION-------------------------------------------
+// function statement
+// function j(){
+//     console.log("hello");
+// }
+// function expression - fucntion is act like a value
+// let o = function(){
+//     console.log("hello 2");
+// }
+// function declaration is also known as function statement
+
+// Anonymous function - a function which dont have any name and its act like a value and we can asseign to a variable
+
+// Named function expression
+
+// Callback function-  a function is passed as a argument
+
+
+// ternary operator
+age = 34;
+let l = age>18? "adult":"not adult";
+console.log(l);
